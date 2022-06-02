d4ad18a412b505b8a64ea2e565915fc8abe68256
unknown <flaquere@yahoo.es>
2022-06-02 19:28:23 +0200

    bootstrap not working

--- fronted/src/app/app.module.ts
+++ fronted/src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { MainComponent } from './components/main/main.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SlideshowComponent } from './components/slideshow/slideshow.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { ProductlistComponent } from './components/productlist/productlist.component';
+//import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    FooterComponent,
+    NavbarComponent,
+    SlideshowComponent,
+    ProductlistComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+     AppRoutingModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
