e40e8bdd22384f5ab37de12c67de3d276490e380
Choephel108 <martipuigcesp@gmail.com>
2022-06-06 12:25:23 +0200

    problemas con css

--- fronted/src/app/app.module.ts
+++ fronted/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { MainComponent } from './components/main/main.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SlideshowComponent } from './components/slideshow/slideshow.component';
+import { ProductlistComponent } from './components/productlist/productlist.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    FooterComponent,
+    NavbarComponent,
+    SlideshowComponent,
+    ProductlistComponent
   ],
   imports: [
     BrowserModule,
