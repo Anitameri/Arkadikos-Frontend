11455292a15e17d6ddcec875f19dfcdfb2f83eac
UndefinedBehaviourAndIllFormed <sardevcpp@gmail.com>
2022-06-21 11:39:30 +0200

    first checkout

--- src/app/components/pages/checkout/checkout.component.ts
+++ src/app/components/pages/checkout/checkout.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { order } from 'app/interface/order';
+import { ProductService } from 'app/services/product.service';
 
 @Component({
   selector: 'app-checkout',
@@ -6,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./checkout.component.css']
 })
 export class CheckoutComponent implements OnInit {
+  orders:order[]=[];
+  items:number=0;
+  totalPrice:number=0;
 
-  constructor() { }
+  constructor(private service:ProductService) { }
 
   ngOnInit(): void {
+    this.orders = this.service.orders;
+    this.items = this.orders.length;
+    for(let o of this.orders)
+      if(o.totalPrice)
+        this.totalPrice += o.totalPrice;
   }
 
 }
